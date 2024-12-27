2b94ce93f8f5654408a87c7b57518f339f7008c4
roshankhanxox <64058135+roshankhanxox@users.noreply.github.com>
2024-12-27 18:43:16 +0530

    eslint solved

--- my-app/eslint.config.mjs
+++ my-app/eslint.config.mjs
@@ -6,11 +6,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.config({
+		extends: ["next"],
+		rules: {
+			"react/no-unescaped-entities": "off",
+			"@next/next/no-page-custom-font": "off",
+			"@typescript-eslint/no-unused-vars": "off",
+			"@typescript-eslint/no-explicit-any": "off",
+		},
+	}),
 ];
 
 export default eslintConfig;
